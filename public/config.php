430a605e61ba5629f084049f7b13b5350f495ac5
Tom Slominski <me@tomslominski.net>
2013-02-27 21:45:49 +0000

    1.4

--- public/config.php
+++ public/config.php
@@ -4,16 +4,16 @@
 ISQ::$general = array(
 	'name' => 'kwl.me', // The name of your URL shortener
 	'qr' => 1, // Do you want to display a QR code?
-	'mobile' => 1 // Do you want to display a QR code?
+	'mobile' => 1 // Do you want to enable the mobile version?
 );
 
 // Menu- The width of ∞² menu allows you to have about 10 links
 // Leave empty to disable
 ISQ::$links = array(
-	'name_1' => 'Blog',
+	'name_1' => 'Tom Slominski',
 	'url_1' => 'http://tomslominski.net/',
 	'name_2' => 'Get ∞²!',
-	'url_2' => 'http://tomslominski.net/infinity-squared/',
+	'url_2' => 'https://github.com/tomslominski/infinity-squared/',
 	'name_3' => '',
 	'url_3' => '',
 	'name_4' => '',
