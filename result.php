2423d8036f1e8bdb11ca5373a6dd09673fb2860f
Tom Slominski <tomslominski@gmail.com>
2013-11-24 21:35:51 +0000

    Moving up some definitions to fix the bookmarklets (thanks Sam Tran!)

--- result.php
+++ result.php
@@ -1,12 +1,22 @@
 <?php
 include('header.php');
 
+$url     = yourls_sanitize_url( $_REQUEST['url'] );
+$keyword = isset( $_REQUEST['keyword'] ) ? yourls_sanitize_keyword( $_REQUEST['keyword'] ): '' ;
+$title   = isset( $_REQUEST['title'] ) ? yourls_sanitize_title( $_REQUEST['title'] ) : '' ;
+$return  = yourls_add_new_link( $url, $keyword, $title );
+		
+$shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
+$message  = isset( $return['message'] ) ? $return['message'] : '';
+$title    = isset( $return['title'] ) ? $return['title'] : '';
+
 // Stop here if bookmarklet with a JSON callback function ("instant" bookmarklets) -- code from YOURLS
 if( isset( $_GET['jsonp'] ) && $_GET['jsonp'] == 'yourls' ) {
 	$short = $return['shorturl'] ? $return['shorturl'] : '';
 	$message = "Short URL (Ctrl+C to copy)";
 	header('Content-type: application/json');
 	echo yourls_apply_filter( 'bookmarklet_jsonp', "yourls_callback({'short_url':'$short','message':'$message'});" );
+	include('footer.php');
 
 	die();
 }
@@ -20,15 +30,6 @@ if (!$resp->is_valid) {
 	// What happens when the CAPTCHA was entered incorrectly
 	die ( '<p class="error" title="' . $resp->error . '">' . yourls__( 'The reCAPTCHA wasn\'t entered correctly. Go back and try it again.', 'isq_translation' ) . '</p></div></div>' );
 }
-
-$url     = yourls_sanitize_url( $_REQUEST['url'] );
-$keyword = isset( $_REQUEST['keyword'] ) ? yourls_sanitize_keyword( $_REQUEST['keyword'] ): '' ;
-$title   = isset( $_REQUEST['title'] ) ? yourls_sanitize_title( $_REQUEST['title'] ) : '' ;
-$return  = yourls_add_new_link( $url, $keyword, $title );
-		
-$shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
-$message  = isset( $return['message'] ) ? $return['message'] : '';
-$title    = isset( $return['title'] ) ? $return['title'] : '';
 
 ?>
 
