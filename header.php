ce033d8e76d855639634c2b28627bd43cf353c97
Tom Slominski <tomslominski@gmail.com>
2015-03-02 20:32:25 +0000

    App icons for iOS, Windows

--- header.php
+++ header.php
@@ -70,6 +70,28 @@ $(document).ready(function()
 	});
 });
 </script>
+
+<!-- App icons
+    Generated using http://realfavicongenerator.net -->
+<link rel="apple-touch-icon" sizes="57x57" href="public/images/app-icons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="public/images/app-icons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="public/images/app-icons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="public/images/app-icons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="public/images/app-icons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="public/images/app-icons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="public/images/app-icons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="public/images/app-icons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="public/images/app-icons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="public/images/app-icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="public/images/app-icons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="public/images/app-icons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="public/images/app-icons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="public/images/app-icons/manifest.json">
+<link rel="shortcut icon" href="public/images/app-icons/favicon.ico">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-TileImage" content="public/images/app-icons/mstile-144x144.png">
+<meta name="msapplication-config" content="public/images/app-icons/browserconfig.xml">
+<meta name="theme-color" content="#013f6d">
 </head>
 
 <body>
