e46535bc645b9d7e90294dd6dfd65df2375d521a
Tom Slominski <tomslominski@gmail.com>
2015-03-06 17:02:19 +0000

    Further style changes

--- index.php
+++ index.php
@@ -8,24 +8,27 @@ $title   = isset( $_REQUEST['title'] ) ? yourls_sanitize_title( $_REQUEST['title
 
 <div class="content">
 	<h2><?php yourls_e( 'Enter a new URL to shorten', 'isq_translation'); ?></h2>
-	<h3><?php yourls_e( 'Hover over the labels to see more information', 'isq_translation'); ?></h3>
-	<form method="post" action="result.php">
+	<form method="post" action="result.php" class="newurl">
 		<div class="form-item full-width">
 			<label for="url" class="primary"><?php yourls_e( 'Long URL (required):', 'isq_translation'); ?></label>
 			<label for="url" class="secondary"><?php yourls_e( 'Paste the long URL here', 'isq_translation'); ?></label>
 			<input type="url" id="url" name="url" value="<?php echo($url); ?>" />
 		</div>
 
-		<div class="form-item half-width">
+		<div class="halves">
+
+		<div class="form-item half-width left">
 			<label for="keyword" class="primary"><?php yourls_e( 'Custom keyword:', 'isq_translation'); ?></label>
 			<label for="keyword" class="secondary"><?php yourls_e( 'A keyword replaces the default short string', 'isq_translation'); ?></label>
 			<input type="text" id="keyword" name="keyword" value="<?php echo($keyword); ?>" />
 		</div>
 
-		<div class="form-item half-width">
+		<div class="form-item half-width right">
 			<label for="title" class="primary"><?php yourls_e( 'Optional title:', 'isq_translation'); ?></label>
 			<label for="title" class="secondary"><?php yourls_e( 'Optional title used when sharing a link from YOURLS using social sharers', 'isq_translation'); ?></label>
 			<input type="text" id="title" value="<?php echo($title); ?>" />
+		</div>
+		
 		</div>
 
 		<div class="form-item recaptcha-container">
