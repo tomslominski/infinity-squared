f4265e77a9716231451403e80bd19ae636a4ef61
Tom Slominski <tomslominski@gmail.com>
2015-03-22 14:58:56 +0000

    Small screen optimisation

--- header.php
+++ header.php
@@ -25,7 +25,7 @@ isq_load_textdomain();
 		<title><?php echo ISQ::$general['name']; ?></title> <!-- Site title defined in theme settings -->
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="<?php echo YOURLS_SITE; ?>/public/style.css" /><!-- Theme CSS -->
 
 		<!-- App icons generated using http://realfavicongenerator.net -->
